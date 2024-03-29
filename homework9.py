00cfaec58e1b39ac8dfc941f3e526b3782bf1099
NATACHAOS <164019299+NATACHAOS@users.noreply.github.com>
2024-03-29 13:20:59 +0300

    Add files via upload

---
+++ homework9.py
@@ -0,0 +1,16 @@
+def print_params(params):
+    print(params, params)
+
+
+print_params("Настя")
+print_params("Вася")
+print_params("Стася")
+
+
+def print_params(params):
+    print((params + ' ') * 2)
+
+
+print_params("Настя")
+print_params("Вася")
+print_params("Стася")
